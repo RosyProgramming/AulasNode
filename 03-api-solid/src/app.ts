c96586e21c192f575aa233c5551ecd1ba6bcbb2e
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-02-13 21:08:19 -0300

    api solid

--- 03-api-solid/src/app.ts
+++ 03-api-solid/src/app.ts
@@ -1,3 +1,16 @@
+/* eslint-disable prettier/prettier */
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client' 
 
 export const app = fastify()
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: 'Rosana',
+        email: "rosana@teste.com"
+    },
+})
+
+// ORM - Object Relacional Mapper 
+
