00b1b95a68c41b9fa0c480c1864868bc4fb4bd0b
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-02-28 21:23:56 -0300

    Caso de uso validar o check-in

--- 03-api-solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ 03-api-solid/src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -6,6 +6,16 @@ import { randomUUID } from 'node:crypto'
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = []
 
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id)
+
+    if (!checkIn) {
+      return null
+    }
+
+    return checkIn
+  }
+
   async findByUserIdOnDate(userId: string, date: Date) {
    // const checkInOnSameDate = this.items.find(
      // (checkIn) => checkIn.user_id === userId,
@@ -49,6 +59,16 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     }
 
     this.items.push(checkIn)
+
+    return checkIn
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id)
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn
+    }
 
     return checkIn
   }
