c760f3688a2c036399fec312303af0adb0b04c5d
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-03-08 15:50:20 -0300

    Princípios da autenticação JWT

--- 03-api-solid/src/http/routes.ts
+++ 03-api-solid/src/http/routes.ts
@@ -1,10 +1,14 @@
 import { FastifyInstance } from "fastify"
 import { authenticate } from "./controllers/authenticate"
 import { register } from "./controllers/register"
+import { profile } from '@/http/controllers/profile'
 
 export async function appRoutes(app:FastifyInstance){
     // criando uma autenticação de login
     app.post('/users', register)
 
     app.post('/sessions', authenticate)
+
+    /** Authenticated */
+   app.get('/me', profile)
 }
