4453397f325e95381d9dd0e621b4db233f842f6a
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-03-09 12:34:45 -0300

    Controller de perfil

--- 03-api-solid/src/http/controllers/profile.ts
+++ 03-api-solid/src/http/controllers/profile.ts
@@ -1,10 +1,21 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use.case'
  
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
    
-   await request.jwtVerify()
- 
-   console.log(request.user.sub)
+   // await request.jwtVerify()
+   const getUserProfile = makeGetUserProfileUseCase()
+   
+   const { user } = await getUserProfile.execute({
+      userId: request.user.sub,
+   })
 
-   return reply.status(200).send()
+   // console.log(request.user.sub)
+
+   return reply.status(200).send({
+      user: {
+        ...user,
+        password_hash: undefined,
+      },
+   })
 }
--- 03-api-solid/src/http/routes.ts
+++ 03-api-solid/src/http/routes.ts
@@ -2,6 +2,7 @@ import { FastifyInstance } from "fastify"
 import { authenticate } from "./controllers/authenticate"
 import { register } from "./controllers/register"
 import { profile } from '@/http/controllers/profile'
+import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
 export async function appRoutes(app:FastifyInstance){
     // criando uma autenticação de login
@@ -10,5 +11,6 @@ export async function appRoutes(app:FastifyInstance){
     app.post('/sessions', authenticate)
 
     /** Authenticated */
-   app.get('/me', profile)
+   // app.get('/me', profile)
+   app.get('/me', { onRequest: [verifyJwt] }, profile)
 }
