51fc5e9748e1cbbcd59edb72fa3a1034f8da3ca2
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-03-11 22:01:54 -0300

    Controller de criação de academia

--- 03-api-solid/src/http/controllers/users/routes.ts
+++ 03-api-solid/src/http/controllers/users/routes.ts
@@ -1,10 +1,10 @@
 import { FastifyInstance } from "fastify"
-import { authenticate } from "./controllers/authenticate"
-import { register } from "./controllers/register"
-import { profile } from '@/http/controllers/profile'
+import { authenticate } from "./authenticate"
+import { register } from "./register"
+import { profile } from './profile'
 import { verifyJwt } from '@/http/middlewares/verify-jwt'
 
-export async function appRoutes(app:FastifyInstance){
+export async function usersRoutes(app:FastifyInstance){
     // criando uma autenticação de login
     app.post('/users', register)
 
