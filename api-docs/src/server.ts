1f91256015f363bd75c0c224c009f86e11f97495
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-06-04 22:27:23 -0300

    configuração  do fastify e utilização do scalar UI

---
+++ api-docs/src/server.ts
@@ -0,0 +1,32 @@
+import { fastify } from "fastify";
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import scalarUI from '@scalar/fastify-api-reference'
+
+const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Example API Docs',
+      version: '1.0.0',
+    }
+  }
+});
+
+app.get('/openapi.json', () => app.swagger());
+
+app.register(scalarUI, {
+    routePrefix: '/docs',
+    configuration: {
+        layout: 'modern'
+    }
+})
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!')
+})
