17b4e2617d62a6fb97a503e46d5473d86e5fc3d9
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-06-05 22:21:55 -0300

    geração de dados estruturados

--- ai-sdk/src/app/api/ai/route.ts
+++ ai-sdk/src/app/api/ai/route.ts
@@ -1,13 +1,19 @@
-import { NextRequest, NextResponse } from "next/server";
-import { openai } from '@ai-sdk/openai';
-import { generateText } from "ai";
+import { openrouter } from "@/ai/open-route";
+import { generateObject } from "ai";
+import { NextResponse, type NextRequest } from "next/server";
+import { z } from 'zod'
 
-export async function GET (request: NextRequest){
-    const result = await generateText({
-        model: openai('gpt-4o'),
-        prompt: 'Traduza "Hello word" para português!',
-        system: 'Você é uma AI especializada em tradução, sempre retorne da maneira sucinta possível.'
-    })
+export async function GET(request: NextRequest) {
+  const result = await generateObject({
+    model: openrouter.chat('openai/gpt-4o-2024-11-20'),
+    schema: z.object({
+      pt: z.string().describe("Tradução para português"),
+      fr: z.string().describe("Tradução para francês"),
+      es: z.string().describe("Tradução para espanhol"),
+    }),
+    prompt: 'Traduza "Hello World" para português!',
+    system: 'Você é uma AI especializada em tradução, sempre retorne da maneira sucinta possível.'
+  })
 
-    return NextResponse.json({ message: result.text })
+  return NextResponse.json({ message: result.object })
 }
