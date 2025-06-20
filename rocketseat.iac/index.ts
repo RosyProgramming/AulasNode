3beec5b18529155ce6cfd856204a49ba6ec047db
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-06-20 15:34:57 -0300

    reinstalei o pulimi

--- rocketseat.iac/index.ts
+++ rocketseat.iac/index.ts
@@ -1,9 +1,12 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
+const bucket = new aws.s3.BucketV2("primeiro-teste-pos-rocketseat-teste", {
+    bucket: 'primeiro-teste-pos-rocketseat-teste',
+    tags: {
+        IAC: "true",
+    }
+});
 
-// Export the name of the bucket
 export const bucketName = bucket.id;
+export const bucketInfo =  bucket.bucket;
+export const bucketArn  = bucket.arn;
