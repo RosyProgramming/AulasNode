a4db82c42f05f52b371cb10c43a98c5df44f2b0f
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-09-04 18:22:00 -0300

    teste avançados

--- rocketseat.iac/index.ts
+++ rocketseat.iac/index.ts
@@ -1,12 +1,34 @@
 import * as aws from "@pulumi/aws";
 
-const bucket = new aws.s3.BucketV2("primeiro-teste-pos-rocketseat-teste", {
-    bucket: 'primeiro-teste-pos-rocketseat-teste',
+const firstBucket  = new aws.s3.BucketV2("primeiro-bucket-01", {
+    bucket: 'primeiro-bucket-pos-full-stck',
     tags: {
         IAC: "true",
     }
 });
 
-export const bucketName = bucket.id;
-export const bucketInfo =  bucket.bucket;
-export const bucketArn  = bucket.arn;
+const secondBucket = new aws.s3.BucketV2("segundo-bucket-02", {
+    bucket: 'segundo-bucket-pos-full-stack',
+    tags: {
+        IAC: "true",
+    },
+});
+
+const ecr = new aws.ecr.Repository("primeiro-ecr-01", {
+    name: 'primeiro-ecr-01',
+    imageTagMutability: 'IMMUTABLE',
+    tags: {
+        IAC: "true",
+    }
+});
+
+export const firstBucketName = firstBucket.id;
+export const firstBucketRegion = firstBucket.region;
+export const firstBucketArn = firstBucket.arn;
+
+export const secondBucketName = secondBucket.id;
+export const secondBucketRegion = secondBucket.region;
+export const secondBucketArn = secondBucket.arn;
+
+export const ecrName = ecr.name;
+export const ecrRepositoryUrl = ecr.repositoryUrl;
